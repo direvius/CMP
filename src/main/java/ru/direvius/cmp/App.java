e2d7fcfa228b50c06cfd47d771b6893702b2c9f8
Alexey V. Lavrenuke <direvius@gmail.com>
2012-06-30 00:04:40 +0400

    first try for working client

--- src/main/java/ru/direvius/cmp/App.java
+++ src/main/java/ru/direvius/cmp/App.java
@@ -1,5 +1,11 @@
 package ru.direvius.cmp;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            Socket s = new Socket("10.0.3.70", 688);
+            CMPClient cc = new CMPClient(s.getInputStream(), s.getOutputStream());
+            Thread.sleep(30000);
+            System.exit(0);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 }
