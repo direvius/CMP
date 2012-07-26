d5a4e5d28d485eefdd48645eb6b947f250348c85
Alexey Lavrenuke <direvius@gmail.com>
2012-07-27 03:03:59 +0400

    Test request builder

--- src/main/java/ru/direvius/cmp/App.java
+++ src/main/java/ru/direvius/cmp/App.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.security.GeneralSecurityException;
+import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,7 +22,8 @@ public class App {
             Socket s = new Socket("10.0.3.70", 688);
             CMPClient cc = new CMPClient(s.getInputStream(), s.getOutputStream());
             cc.open();
-            cc.sendEncrypt(Util.hexStringToByteArray("01-01-01-02-04-00-87-82-F8-03-08-09-00-00-00-10-40-92-59-04-04-4F-C8-52-CE"));
+            //cc.sendEncrypt(Util.hexStringToByteArray("01-01-01-02-04-00-87-82-F8-03-08-09-00-00-00-10-40-92-59-04-04-4F-C8-52-CE"));
+            cc.sendEncrypt(new CardInfoRequestBuilder((byte)1, 0x8782F8, 0x0900000010409259L, new Date()).build());
             cc.receiveDecrypt();
             Thread.sleep(5000);
             cc.close();
